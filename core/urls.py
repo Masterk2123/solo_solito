4a251a280a9b69a6aa7a4c1e7f546673d23b3ff9
Masterk2123 <ke.arucutipa@duocuc.cl>
2023-07-10 23:41:01 -0400

    Please

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from .views import home, poblar_bd, producto, tienda_producto, ficha_producto, contacto, registro, sesion, vehiculo, vehiculo_tienda, vehiculo_ficha
+from .views import home, poblar_pro, producto, tienda_producto, ficha_producto, contacto, registro, sesion, vehiculo, vehiculo_tienda, vehiculo_ficha, poblar_ve
 
 urlpatterns = [
     path('', home, name="home"),
-    path('poblar_bd', poblar_bd, name="poblar_bd"),
+    path('poblar_pro', poblar_pro, name="poblar_pro"),
     path('producto/<action>/<id_producto>', producto, name="producto"),
     path('tienda_producto', tienda_producto, name="tienda_producto"),
     path('ficha_producto/<id_producto>', ficha_producto, name="ficha_producto"),
     path('contacto', contacto, name="contacto"),
     path('registro', registro, name="registro"),
     path('sesion', sesion, name="sesion"),
+    path('poblar_ve', poblar_ve, name="poblar_ve"),
     path('vehiculo/<action>/<id>', vehiculo, name="vehiculo"),
     path('vehiculo_tienda', vehiculo_tienda, name="vehiculo_tienda"),
     path('vehiculo_ficha/<id>', vehiculo_ficha, name="vehiculo_ficha"),
--- core/views.py
+++ core/views.py
@@ -51,14 +51,6 @@ def producto(request, action, id_producto):
     data["list"] = Producto.objects.all().order_by('id_producto')
     return render(request, "core/producto.html", data)
 
-
-
-
-
-
-
-
-
 def vehiculo_tienda(request):
     data = {"list": Vehiculo.objects.all().order_by('patente')}
     return render(request, "core/vehiculo_tienda.html", data)
@@ -98,25 +90,25 @@ def vehiculo(request, action, id):
         except:
             data["mesg"] = "¡El vehículo ya estaba eliminado!"
 
-
-
-
-
     data["list"] = Vehiculo.objects.all().order_by('patente')
     return render(request, "core/vehiculo.html", data)
 
-def poblar_bd(request):
+def poblar_pro(request):
     Producto.objects.all().delete()
     Producto.objects.create(id_producto=1, producto='LLANTA ARO 14 587 4X100', detalles="LLANTA ARO 14 587 4X100 BLACK FACE MACHINED POWERCARS", imagen="images/CN839.jpg", categoria=Categoria.objects.get(idCategoria=1))
     Producto.objects.create(id_producto=2, producto='NEUMATICO 245/45ZR20', detalles="	NEUMATICO 245/45ZR20 KAPSEN S2000 103Y", imagen="images/LL0307-2.jpg", categoria=Categoria.objects.get(idCategoria=1))
     Producto.objects.create(id_producto=3, producto='SILICONE FREE DRESSING', detalles="Silicone Free Dressing de Meguiar es la opción ideal para los profesionales que buscan un acondicionador para neumáticos de alto rendimiento que se pueda usar en cualquier entorno sensible a la silicona.", imagen="images/D16101-1.jpg", categoria=Categoria.objects.get(idCategoria=1))
-    Producto.objects.create(id_producto=4, producto='ESPACIADOR 6X135/6X139', detalles="ESPACIADOR 6X135/6X139", imagen="images/ACC61.jpg", categoria=Categoria.objects.get(idCategoria=1))
+    Producto.objects.create(id_producto=4, producto='ESPACIADOR 6X135/6X139', detalles="ESPACIADOR 6X135/6X139", imagen="images/ACC61.jpg", categoria=Categoria.objects.get(idCategoria=1))  
     return redirect(producto, action='ins', id_producto = '-1')
 
+def poblar_ve(request):
+    Vehiculo.objects.all().delete()
     Vehiculo.objects.create(patente="SCAR47", marca='Mustang', modelo="Mustang de 1970", imagen="images/mustang.jpg",servicio="Suspension y Direccion",detalle_servicio="arreglo", categoria=Categoria.objects.get(idCategoria=2))
     Vehiculo.objects.create(patente="TIRO98", marca='Mercedes-Benz', modelo="Iron Bike de 1998", imagen="images/motoiron.jpg",servicio="Suspension y Direccion",detalle_servicio="arreglo", categoria=Categoria.objects.get(idCategoria=3))
     Vehiculo.objects.create(patente="UVAM20", marca='Silver Plus', modelo="Silver de 2000", imagen="images/silver.jpg",servicio="Suspension y Direccion",detalle_servicio="arreglo", categoria=Categoria.objects.get(idCategoria=3))
-    return redirect(vehiculo, action='ins', id = '-1')
+    return redirect(vehiculo, action='ins', id = '-1')  
+
+
 
 
 def contacto(request):
